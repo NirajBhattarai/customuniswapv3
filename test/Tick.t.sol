db7f257b8af0ec5cbc462fb0ae0bb0a7d699b113
NirajBhattarai <nirajbhattarai19@gmail.com>
2026-02-17 17:56:24 +0545

    Add unit test setup for UniswapV3Pool and tick.sol

---
+++ test/Tick.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.30;
+
+import "forge-std/Test.sol";
+import {tickSpacingToMaxLiquidityPerTick} from "../src/math/tick.sol";
+import {MIN_TICK, MAX_TICK} from "../src/math/constants.sol";
+
+contract TickTest is Test {
+    function setUp() external {
+        // Initialize tick test setup here
+        // Add setup logic for tick function tests
+    }
+
+    // Add your test cases below
+}
---
+++ test/UniswapV3Pool.t.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity >=0.8.30;
+
+import "forge-std/Test.sol";
+import {UniswapV3Pool} from "../src/UniswapV3Pool.sol";
+import {IUniswapV3Pool} from "../src/interfaces/IUniswapV3Pool.sol";
+
+contract UniswapV3PoolTest is Test {
+    UniswapV3Pool pool;
+
+    function setUp() external {
+        // Initialize pool setup here
+        // Add setup logic for UniswapV3Pool tests
+    }
+
+    // Add your test cases below
+}
